42b5d953f8a4737c764d21d40dc83b47d61909cf
Simer0996 <singhsimer09@gmail.com>
2022-01-03 16:03:22 -0800

    adding

---
+++ practice2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RecipeList from './RecipeList';
+
+export const ThemeContext = React.createContext()
+function App() {
+return(
+  <RecipeList />
+)
+}
+
+export default App;
+
---
+++ practice2/src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Recipe() {
+    return (
+        <div>
+            <div>
+                <h3>Plain Chicken</h3>
+            </div>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    )
+}
